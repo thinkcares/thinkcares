f3c2b3b871961160db44bae45f5b1d7691e9959a
Benito Sánchez <bsb@thinkcares.com>
2018-03-05 17:05:18 -0600

    tensorflow update examples

---
+++ Machine_Learning/HrdzPython/TF_ML/__init__.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+a = tf.constant([5,3,8])
+b = tf.constant([3,-1,2])
+c = tf.add(a,b)
+print(tf.__version__)
+
+
+print(c)
+with tf.Session() as sess:
+    result = sess.run(c)
+    print(result)
+
+def compute_area(sides):
+    # slice the input to get the sides
+    a = sides[:, 0]  # 5.0, 2.3
+    b = sides[:, 1]  # 3.0, 4.1
+    c = sides[:, 2]  # 7.1, 4.8
+    #  Heron's formula
+    s = (a + b + c) * 0.5  # (a + b) is a short-cut to tf.add(a, b)
+    areasq = s * (s - a) * (s - b) * (s - c)  # (a * b) is a short-cut to tf.multiply(a, b), not tf.matmul(a, b)
+    return tf.sqrt(areasq)
+
+with tf.Session() as sess:
+        # pass in two triangles
+        area = compute_area(tf.constant([
+            [5.0, 3.0, 7.1],
+            [2.3, 4.1, 4.8]
+        ]))
+        result = sess.run(area)
+        print(result)
+
+with tf.Session() as sess:
+  sides = tf.placeholder(tf.float32, shape=(None, 3))  # batchsize number of triangles, 3 sides
+  area = compute_area(sides)
+  result = sess.run(area, feed_dict = {
+      sides: [
+        [5.0, 3.0, 7.1],
+        [2.3, 4.1, 4.8]
+      ]
+    })
+  print(result)
