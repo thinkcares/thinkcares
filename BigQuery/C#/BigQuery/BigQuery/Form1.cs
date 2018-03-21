c933c20dbe702920530dad1219c574ba79ed1502
Benito Sánchez <bsb@thinkcares.com>
2018-03-21 13:12:41 -0600

    SE ACTUALIZA PROY C#

--- BigQuery/C#/BigQuery/BigQuery/Form1.cs
+++ BigQuery/C#/BigQuery/BigQuery/Form1.cs
@@ -18,22 +18,69 @@ namespace BigQuery
 {
     public partial class Form1 : Form
     {
+        public static List<string> lstCmdsSqlExtract = new List<string>();
+        public static List<string> lstCmdsbqCmd = new List<string>();
+        public static List<string> lstCmdsCopyGS = new List<string>();
+        public static string sAuthJasonPathFile = ConfigurationManager.AppSettings["AuthJasonPathFile"];
+        public static GoogleCredential credential = GoogleCredential.FromFile(sAuthJasonPathFile);
+        public static string sProjectID;
+        public static string sDatasetId;
+        public static string sBucketPathToLoadCsvSET;
+
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             DateTime horaInicio = DateTime.Now;
             txtHoraInicio.Text = horaInicio.ToString();
-            string sProjectID = ConfigurationManager.AppSettings["ProjectID"];
-            string sDatasetId = ConfigurationManager.AppSettings["DatasetId"];
-            string sAuthJasonPathFile = ConfigurationManager.AppSettings["AuthJasonPathFile"];
-            string sBucketPathToLoadCsvSET = ConfigurationManager.AppSettings["BucketPathToLoadCsvSET"];
-            
+            InicializaVariables();
+            BuscaArchivosGS(sProjectID, credential); // Busca Files en el bucket destino y borra todos los files en éste
+            BigQueryClean(sProjectID, sDatasetId,credential); // Borra tablas en BigQuery
+            // ExtractSql2Csv(); // Extrae datos de MSSQL a archivos Csv
+            // LimpiaSed(@"C:\tmp"); // Limpia, comprime csvs en 7zip
+            // Upload2GS(); // sube archivos a Google Storage
+            // LoadBq(); // Inserta archivos en tablas de BigQuery
+            // BigQueryUpdate(sProjectID, sDatasetId, credential);
+            DateTime horaFin = DateTime.Now;
+            txtHoraFin.Text = horaFin.ToString();
+            MessageBox.Show("Proceso Finalizado Totalmente");
 
-            List<string> lstCmdsSqlExtract = new List<string>();
+        }
+        public static void LoadBq()
+        {
+            foreach (var bqcmd in lstCmdsbqCmd)
+            {
+                Console.WriteLine(bqcmd.Replace("'", "\""));
+                EjecutarCmd(bqcmd.Replace("'", "\""));
+            }
+        }
+        public static void Upload2GS()
+        {  foreach (var file in lstCmdsCopyGS)
+            {
+                EjecutarCmd("cd /tmp && " + file.Replace("'", "\""));
+            }
+         }
+        public static void ExtractSql2Csv()
+        {
+            foreach (var sql in lstCmdsSqlExtract)
+            {
+                EjecutarCmd("cd /tmp && " + sql.Replace("'", "\""));
+            }
+
+        }
+        public static bool InicializaVariables()
+        {
+            bool bandera = false;
+            try
+            {
+             sProjectID = ConfigurationManager.AppSettings["ProjectID"];
+             sDatasetId = ConfigurationManager.AppSettings["DatasetId"];
+             
+             sBucketPathToLoadCsvSET = ConfigurationManager.AppSettings["BucketPathToLoadCsvSET"];
             foreach (string key in ConfigurationManager.AppSettings)
             {
                 if (key.StartsWith("ExtractMSSQL2csv"))
@@ -41,11 +88,7 @@ namespace BigQuery
                     string value = ConfigurationManager.AppSettings[key];
                     lstCmdsSqlExtract.Add(value);
                 }
-
             }
-
-            
-            List<string> lstCmdsCopyGS = new List<string>();
             foreach (string key in ConfigurationManager.AppSettings)
             {
                 if (key.StartsWith("CopyGS"))
@@ -53,10 +96,7 @@ namespace BigQuery
                     string value = ConfigurationManager.AppSettings[key];
                     lstCmdsCopyGS.Add(value);
                 }
-
             }
-            //bqCmd1
-            List<string> lstCmdsbqCmd = new List<string>();
             foreach (string key in ConfigurationManager.AppSettings)
             {
                 if (key.StartsWith("bqCmd"))
@@ -64,54 +104,16 @@ namespace BigQuery
                     string value = ConfigurationManager.AppSettings[key];
                     lstCmdsbqCmd.Add(value);
                 }
-
             }
-
-
-            var credential = GoogleCredential.FromFile(sAuthJasonPathFile);
-            //var storage = StorageClient.Create(credential);
-            //var bq = StorageClient.Create(credential);
-            // Make an authenticated API request. //var buckets = storage.ListBuckets(projectId);
-            //List<String> BucketList = new List<string>();
-            //foreach (var bucket in buckets)
-            //{
-            //    BucketList.Add(bucket.Name);
-            //}
-            BigQueryClean(sProjectID, sDatasetId,credential);
-
-            //MessageBox.Show("Proceso BigQuery Clean Finalizado");
-            //foreach (var sql in lstCmdsSqlExtract)
-            //{
-            //    EjecutarCmd("cd /tmp && " + sql.Replace("'", "\""));
-            //}
-            
-
-            LimpiaSed(@"C:\tmp");
-
-
-
-            foreach (var file in lstCmdsCopyGS)
+                bandera = true;
+            }
+            catch (Exception ex)
             {
-                EjecutarCmd("cd /tmp && " + file.Replace("'", "\""));
+
+                MessageBox.Show(ex.Message);
             }
-            // MessageBox.Show("Proceso GS Copy Command Finalizado");
-
-            foreach (var bqcmd in lstCmdsbqCmd)
-            {
-                Console.WriteLine(bqcmd.Replace("'", "\""));
-                EjecutarCmd(bqcmd.Replace("'", "\""));
-            }
-            // MessageBox.Show("Proceso bq  Command Finalizado");
-
-
-            DateTime horaFin = DateTime.Now;
-            txtHoraFin.Text = horaFin.ToString();
-            MessageBox.Show("Proceso Finalizado Totalmente");
-            
-
-
+            return bandera;
         }
-
         public static void EjecutarCmd(string comando)
             {
             try
@@ -171,7 +173,6 @@ namespace BigQuery
                 MessageBox.Show(ex.Message);
             }
         }
-
         public static void LimpiaSed(string path)
         {
             try
@@ -211,10 +212,79 @@ namespace BigQuery
                 File.Delete(fi.FullName);
             }
         }
+        public static void BuscaArchivosGS(string sProjectID, GoogleCredential credential)
+        {
+            var storage = StorageClient.Create(credential);
+           // Make an authenticated API request. 
+            var buckets = storage.ListBuckets(sProjectID);
+            List<string> lstFiles = new List<string>();
+            foreach (var bucket in storage.ListObjects(@"hrdz_input", "").Where( o => o.Name.Contains("CsvFilesSet")))
+            {
+                Console.WriteLine(bucket.Name);
+                lstFiles.Add(bucket.Name);
+            }
 
+            if (lstFiles.Count > 0)
+            {
+                DeleteObjectGStorage("hrdz_input", lstFiles, credential);
+                Console.WriteLine("Se eliminaron {0} archivos", lstFiles.Count);
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron Archivos en el Google Storage");
+            }
+
+            
+            
+        }
+        public static  void DeleteObjectGStorage(string bucketName, IEnumerable<string> objectNames, GoogleCredential credential)
+        {
+            var storage = StorageClient.Create(credential);
+
+            foreach (string objectName in objectNames)
+            {
+                storage.DeleteObject(bucketName, objectName);
+                Console.WriteLine($"Deleted {objectName}.");
+            }
+        }
+        public static void BigQueryUpdate(string sProjectID, string sDatasetId, GoogleCredential credential)
+        {
+            var bqClient = BigQueryClient.Create(sProjectID, credential);
+            try
+            {
+                List<BigQueryParameter> parameters = new List<BigQueryParameter>();
+
+                bqClient.ExecuteQuery("update sethdzqa.zimp_fact a  set a.no_doc_sap= trim(a.no_doc_sap) where 1=1"
+                    ,parameters
+                    , new QueryOptions { UseLegacySql = false });
+                bqClient.ExecuteQuery("update sethdzqa.zexp_fact a  set a.no_doc_sap= trim(a.no_doc_sap) where 1=1"
+                   , parameters
+                   , new QueryOptions { UseLegacySql = false });
+                bqClient.ExecuteQuery("update sethdzqa.det_arch_transfer a  set a.nom_arch= trim(a.nom_arch) where 1=1"
+                   , parameters
+                   , new QueryOptions { UseLegacySql = false });
+                bqClient.ExecuteQuery("update sethdzqa.hist_movimiento a  set a.no_docto= trim(a.no_docto) where 1=1"
+                   , parameters
+                   , new QueryOptions { UseLegacySql = false });
+                bqClient.ExecuteQuery("update sethdzqa.hist_solicitud a  set a.no_docto= trim(a.no_docto) where 1=1"
+                , parameters
+                , new QueryOptions { UseLegacySql = false });
+                bqClient.ExecuteQuery("update sethdzqa.movimiento a  set a.no_docto= trim(a.no_docto) where 1=1"
+                , parameters
+                , new QueryOptions { UseLegacySql = false });
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
     }
 }
